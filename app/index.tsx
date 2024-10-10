78e22913a12159c221fbaa3f3942c3b44468ea70
Aryog <yogesh000aryal@gmail.com>
2024-10-10 22:47:05 +0545

    assets added

---
+++ app/index.tsx
@@ -0,0 +1,7 @@
+import { Text } from "react-native-reanimated/lib/typescript/Animated"
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const Home = () => {
+  return (<SafeAreaView><Text> Home </Text></SafeAreaView>)
+}
+export default Home;
