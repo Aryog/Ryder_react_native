67ea10082be7162c2ce98efd2986790a77476c8a
Aryog <yogesh000aryal@gmail.com>
2024-10-11 23:13:16 +0545

    pages added

---
+++ app/(auth)/sign-up.tsx
@@ -0,0 +1,7 @@
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Text } from "react-native";
+
+const SignUn = () => {
+  return (<SafeAreaView><Text> Sign Up</Text></SafeAreaView>)
+}
+export default SignUn;
---
+++ app/(root)/(tabs)/home.tsx
@@ -0,0 +1,7 @@
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Text } from "react-native";
+
+const home = () => {
+	return (<SafeAreaView><Text> Home </Text></SafeAreaView>)
+}
+export default home;
