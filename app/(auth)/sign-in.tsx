c9fe022f4386c719b99ad25aad10a69df6906171
Aryog <yogesh000aryal@gmail.com>
2024-10-16 21:13:59 +0545

    auth sign in and sign up

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,16 +1,41 @@
-import { ScrollView, View, Image, Text } from "react-native";
+import { ScrollView, View, Image, Text, Alert } from "react-native";
 import { images, icons } from '@/constants';
+import { useSignIn } from '@clerk/clerk-expo'
 import InputField from "@/components/InputField";
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 import CustomButton from "@/components/CustomButton";
-import { Link } from "expo-router";
+import { Link, useRouter } from "expo-router";
 import OAuth from "@/components/OAuth";
 const SignIn = () => {
-
+  const { signIn, setActive, isLoaded } = useSignIn()
+  const router = useRouter()
   const [form, setForm] = useState({ email: '', password: '' })
-  const onSignInPress = async () => {
+  const onSignInPress = useCallback(async () => {
+    if (!isLoaded) {
+      return
+    }
 
-  }
+    try {
+      const signInAttempt = await signIn.create({
+        identifier: form.email,
+        password: form.password,
+      })
+
+      if (signInAttempt.status === 'complete') {
+        await setActive({ session: signInAttempt.createdSessionId })
+        router.replace('/(root)/(tabs)/home')
+        console.log("Signed in successfully");
+      } else {
+        // See https://clerk.com/docs/custom-flows/error-handling
+        // for more info on error handling
+        console.error(JSON.stringify(signInAttempt, null, 2))
+      }
+    } catch (err: any) {
+      Alert.alert('Error', err.errors[0].longMessage)
+      console.error(JSON.stringify(err, null, 2))
+    }
+  }, [isLoaded, form.email, form.password])
+
   return (<ScrollView className='flex-1 bg-white'>
     <View className='flex-1 bg-white'>
       <View className="relative w-full h-[250px]">
---
+++ app/(root)/(tabs)/_layout.tsx
@@ -0,0 +1,19 @@
+import { Tabs } from "expo-router"
+import { Image, View } from "react-native"
+const TabIcon = () => {
+	<View>
+		<View>
+			<Image src="" />
+		</View>
+	</View>
+}
+const Layout = () => {
+	return (
+		<Tabs initialRouteName="index" screenOptions={{ tabBarActiveTintColor: 'white' }}>
+			<Tabs.Screen
+				name="home" options={{ title: "Home", headerShown: false, tabBarIcon: () => <TabIcon /> }}
+			/>
+		</Tabs>
+	)
+}
+export default Layout;
