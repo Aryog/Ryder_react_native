27f8635ece07f6c202f6349b20bb680c2dca5710
Aryog <yogesh000aryal@gmail.com>
2024-10-19 14:19:25 +0545

    location wise home page added

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -8,6 +8,7 @@ import CustomButton from "@/components/CustomButton";
 import InputField from "@/components/InputField";
 import OAuth from "@/components/OAuth";
 import { icons, images } from "@/constants";
+import { fetchAPI } from "@/lib/fetch";
 
 const SignUp = () => {
   const { isLoaded, signUp, setActive } = useSignUp();
@@ -50,6 +51,9 @@ const SignUp = () => {
         code: verification.code,
       });
       if (completeSignUp.status === "complete") {
+
+        // TODO: Create a database user
+        await fetchAPI('/(api)/user', { method: "POST", body: JSON.stringify({ name: form.name, email: form.email, clerkId: completeSignUp.createdUserId }) })
 
         await setActive({ session: completeSignUp.createdSessionId });
         setVerification({
